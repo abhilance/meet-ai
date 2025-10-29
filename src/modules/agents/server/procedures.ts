f0ad46c6f7d5e5f39e5f804b0a2a5f39f95a7acb
Abhishek Bhandari <abhishekbhandari017360@gmail.com>
2025-10-29 11:43:06 +0530

    14 agent page

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -6,13 +6,22 @@ import { agentsInsertionSchema } from "../schema"
 import{z}from "zod"
 import{and, count, desc, eq, getTableColumns, ilike, sql}from "drizzle-orm"
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MIN_PAGE_SIZE, MAX_PAGE_SIZE } from "@/constants";
+import { NOTFOUND } from "dns"
 
 export const agentsRouter = createTRPCRouter({
-    getOne:protectedProcedure.input(z.object({id: z.string()})).query(async({input})=>{
+    getOne:protectedProcedure.input(z.object({id: z.string()})).query(async({input,ctx})=>{
       const [existingAgents] = await db.select({
         meetingCount: sql<number>`5`,
         ...getTableColumns(agents),
-      }).from(agents).where(eq(agents.id, input.id))
+      }).from(agents).where(
+        and(
+          eq(agents.id, input.id),
+          eq(agents.userId,ctx.auth.user.id)
+        ))
+
+        if(!existingAgents){
+          throw new TRPCError({code:"NOT_FOUND", message:"Agent not found"})
+        }
   
       return existingAgents;
     }),
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -8,8 +8,10 @@ import { columns} from "../components/columns"
 import { EmptyState } from "@/components/empty-state";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { DataPagination } from "../components/data-pagination";
+import { useRouter } from "next/navigation";
 
 export const AgentsView =()=>{
+    const router = useRouter();
     const[filters, setfilters]=useAgentsFilters();
     const trpc = useTRPC();
     const {data}= useSuspenseQuery( trpc.agents.getMany.queryOptions({
@@ -20,7 +22,9 @@ export const AgentsView =()=>{
       
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
            <DataTable
-           data={data.items} columns={columns}/>
+           data={data.items} columns={columns}
+           onRowClick={(row)=>router.push(`/agents/${row.id}`)}
+           />
            <DataPagination
            page={filters.page}
            totalPages={data.totalPages}
